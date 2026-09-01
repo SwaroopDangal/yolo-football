f1b278792e63a186613f2d0beca72f72a33a3d39
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-01 18:05:22 +0545

    integrated tracking class in main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     #read video
     video_frames = read_video("video/08fd33_4.mp4")
+
+    #Initalize Tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
+
 
     # Save video
     save_video(video_frames, "output/output_video.avi")
