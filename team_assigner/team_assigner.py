007b7331844cd77c5b63c8b895a47f13f51ed0cd
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-04 18:25:18 +0545

    get clustering model function

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -5,6 +5,17 @@ class TeamAssigner:
         self.team_colors = {}
         self.player_team_dict = {}
 
+        
+    def get_clustering_model(self,image):
+        # Reshape the image to 2D array
+        image_2d = image.reshape(-1,3)
+
+        # Preform K-means with 2 clusters
+        kmeans = KMeans(n_clusters=2, init="k-means++",n_init=1)
+        kmeans.fit(image_2d)
+
+        return kmeans
+
     def assign_team_color(self,frame, player_detections):
         
         player_colors = []
