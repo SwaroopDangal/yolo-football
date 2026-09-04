ceea20b0d6064db27f52586c81d4a08af49561cf
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-04 18:25:50 +0545

    get_player_color function

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -16,6 +16,30 @@ class TeamAssigner:
 
         return kmeans
 
+
+    def get_player_color(self,frame,bbox):
+        image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
+
+        top_half_image = image[0:int(image.shape[0]/2),:]
+
+        # Get Clustering model
+        kmeans = self.get_clustering_model(top_half_image)
+
+        # Get the cluster labels forr each pixel
+        labels = kmeans.labels_
+
+        # Reshape the labels to the image shape
+        clustered_image = labels.reshape(top_half_image.shape[0],top_half_image.shape[1])
+
+        # Get the player cluster
+        corner_clusters = [clustered_image[0,0],clustered_image[0,-1],clustered_image[-1,0],clustered_image[-1,-1]]
+        non_player_cluster = max(set(corner_clusters),key=corner_clusters.count)
+        player_cluster = 1 - non_player_cluster
+
+        player_color = kmeans.cluster_centers_[player_cluster]
+
+        return player_color
+
     def assign_team_color(self,frame, player_detections):
         
         player_colors = []
