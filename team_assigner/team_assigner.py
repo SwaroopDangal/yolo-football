7426bb78cc12b464c8f16709ce573f704bd1178b
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-04 18:24:46 +0545

    assign team colors function

---
+++ team_assigner/team_assigner.py
@@ -0,0 +1,22 @@
+from sklearn.cluster import KMeans
+
+class TeamAssigner:
+    def __init__(self):
+        self.team_colors = {}
+        self.player_team_dict = {}
+
+    def assign_team_color(self,frame, player_detections):
+        
+        player_colors = []
+        for _, player_detection in player_detections.items():
+            bbox = player_detection["bbox"]
+            player_color =  self.get_player_color(frame,bbox)
+            player_colors.append(player_color)
+        
+        kmeans = KMeans(n_clusters=2, init="k-means++",n_init=10)
+        kmeans.fit(player_colors)
+
+        self.kmeans = kmeans
+
+        self.team_colors[1] = kmeans.cluster_centers_[0]
+        self.team_colors[2] = kmeans.cluster_centers_[1]
