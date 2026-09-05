4f2639b605f14aa5aa4c88357097748d7ed3326d
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-05 18:21:23 +0545

    assigning team module integrated in main.py

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 from utils import read_video, save_video
 from trackers import Tracker
+from team_assigner import TeamAssigner
 import cv2
 
 def main():
     #read video
     video_frames = read_video("video/08fd33_4.mp4")
+    print(f"Video read complete.")
 
     #Initalize Tracker
     tracker = Tracker('models/best.pt')
+    print("Tracking objects in video...")
 
     tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
+    print(f"Tracking completed.")
+    print("Assigning teams to players...")
 
-    # save cropped image of player
-    for track_id, player in tracks['players'][0].items():
-        bbox = player['bbox']
-        frame = video_frames[0]
+    # Assign Teams
+    team_assigner = TeamAssigner() 
+    team_assigner.assign_team_color(video_frames[0],tracks['players'][0])
 
-        #crop box from frame
-        cropped_image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
-        #save cropped image
-        cv2.imwrite(f'output/cropped_image.jpg', cropped_image)
+    print(f"Team assignment completed.")
 
-        break
-
-
-    
+    print(f"Drawing annotations on video frames...")
 
     ##draw o/p
     # draw object tracks
 
     output_video_frames=tracker.draw_annotations(video_frames,tracks)
 
+    print(f"Drawing completed.")
+    print(f"Saving output video...")
 
 
     # Save video
