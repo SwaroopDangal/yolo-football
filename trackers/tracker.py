e28c640bf9b9090541c7dc82b1bf688f0c800148
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-05 18:26:20 +0545

    interpolate ball positions

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -4,6 +4,7 @@ import pickle
 import os
 import cv2
 import sys 
+import pandas as pd
 import numpy as np
 from utils import get_center_of_bbox, get_bbox_width, get_foot_position
 sys.path.append('../')
@@ -13,6 +14,17 @@ class Tracker:
     def __init__(self,model_path):
         self.model=YOLO(model_path)
         self.tracker = sv.ByteTrack()
+
+    def interpolate_ball_positions(self,ball_positions):
+        ball_positions = [x.get(1,{}).get('bbox',[]) for x in ball_positions]
+        df_ball_positions = pd.DataFrame(ball_positions,columns=['x1','y1','x2','y2'])
+        #Interpolate missing values
+        df_ball_positions = df_ball_positions.interpolate()
+        df_ball_positions = df_ball_positions.bfill()
+        ball_positions = [{1: {"bbox":x}} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
+
 
     def detect_frames(self,frames):
         batch_size=20
