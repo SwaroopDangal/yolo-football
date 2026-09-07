13cfac5474199b3434eab6a7e181f4bd231ff128
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-07 18:27:55 +0545

    draw possession rectangle function

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -203,8 +203,27 @@ class Tracker:
 
         return frame
 
+    def draw_team_ball_control(self,frame,frame_num,team_ball_control):
+        # Draw a semi-transparent rectangle 
+        overlay = frame.copy()
+        cv2.rectangle(overlay, (1350, 850), (1900,970), (255,255,255), -1 )
+        alpha = 0.4
+        cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+
+        team_ball_control_till_frame = team_ball_control[:frame_num+1]
+        # Get the number of time each team had ball control
+        team_1_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==1].shape[0]
+        team_2_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==2].shape[0]
+        team_1 = team_1_num_frames/(team_1_num_frames+team_2_num_frames)
+        team_2 = team_2_num_frames/(team_1_num_frames+team_2_num_frames)
+
+        cv2.putText(frame, f"Team 1 Possession: {team_1*100:.2f}%",(1400,900), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+        cv2.putText(frame, f"Team 2 Possession: {team_2*100:.2f}%",(1400,950), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+
+        return frame
+
     
-    def draw_annotations(self,video_frames,tracks):
+    def draw_annotations(self,video_frames,tracks,team_ball_control):
         output_video_frames=[]
         for frame_num,frame in enumerate(video_frames):
             frame = frame.copy()
@@ -229,6 +248,7 @@ class Tracker:
             for track_id,ball in ball_dict.items():
                 bbox = ball['bbox']
                 frame = self.draw_traingle(frame,bbox,(0,255,0))
+
 
             output_video_frames.append(frame)
         
