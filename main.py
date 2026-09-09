1be31e8149a109148151bb265143a473bbedac3c
SwaroopDangal <swaroopdangal732@gmail.com>
2026-09-09 18:09:16 +0545

    drawing camera movement

--- main.py
+++ main.py
@@ -69,6 +69,10 @@ def main():
 
     output_video_frames=tracker.draw_annotations(video_frames,tracks,team_ball_control)
 
+      ## Draw Camera movement
+    output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+
+
     print(f"Drawing completed.")
     print(f"Saving output video...")
 
